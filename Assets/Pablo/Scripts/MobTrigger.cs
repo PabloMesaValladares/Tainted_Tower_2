dd555776cd1cb83394e03be05b95752d48932487
PabloMesaValladares <pmesavalladares@gmail.com>
2023-03-22 12:06:57 +0100

    EnemigosUpgrade

--- Assets/Pablo/Scripts/MobTrigger.cs
+++ Assets/Pablo/Scripts/MobTrigger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class PoisonMist : MonoBehaviour
+public class MobTrigger : MonoBehaviour
 {
     //[SerializeField] private GameObject _player;
 
