4e20726fd3c950b42a81e8f633ff3afec20138b9
BabyScylla69 <alvarosilvalobos99@gmail.com>
2023-04-17 13:26:47 +0200

    17/04/2023 2

--- Assets/Scripts/Player/Spells/PillarAbility.cs
+++ Assets/Scripts/Player/Spells/PillarAbility.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 
 public class PillarAbility : MonoBehaviour
 {
-
     [SerializeField] private bool hasOffset;
-    [SerializeField] private float offset, knockBackForce, knowckBackRadius,upKnockBackForce;
+    [SerializeField] private float timer, knockBackForce, knowckBackRadius,upKnockBackForce;
 
-    Timer _timer;
+    Timer[] _timer;
     new Collider collider;
+    Animator _anim;
 
     Vector3 spherePos;
 
     private void Awake()
     {
+        _timer = GetComponents<Timer>();
         collider = GetComponent<Collider>();
-        _timer = GetComponent<Timer>();
+        _anim = GetComponentInChildren<Animator>();
     }
 
     void OnEnable()
     {
-        if(hasOffset)
-        {
-            collider.enabled = false;
-            _timer.StartTimer(offset);
-        }
+        collider.isTrigger = true;
+
+        if (hasOffset)
+            StartTimer(timer, 0);
         else
             PlayAnimation();
     }
@@ -35,17 +35,26 @@ public class PillarAbility : MonoBehaviour
         if(other.TryGetComponent<PlayerController>(out PlayerController player))
         {
             spherePos = player.transform.position;
-            player.GetComponent<Rigidbody>().AddExplosionForce(knockBackForce, spherePos, knowckBackRadius, upKnockBackForce);
         }
     }
 
     public void PlayAnimation()
     {
-
+        _anim.SetTrigger("Pilar");
     }
 
     public void ChangeColliderType()
     {
         collider.isTrigger = false;
+    }
+
+    public void DisableParent()
+    {
+        transform.parent.gameObject.SetActive(false);
+    }
+
+    public void StartTimer(float timer, int i)
+    {
+        _timer[i].StartTimer(timer);
     }
 }
