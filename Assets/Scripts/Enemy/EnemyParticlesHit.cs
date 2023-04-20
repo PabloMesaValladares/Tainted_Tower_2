67e6174bf7ce06a566606cf729e9ec0e63a0e38f
Mario Marcos Navarro <mmarnav28@gmail.com>
2023-04-20 13:26:49 +0200

    PiromanticEnemy v.1.1

---
+++ Assets/Scripts/Enemy/EnemyParticlesHit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyParticlesHit : MonoBehaviour
+{
+    public ParticleSystem enemyParticleAttack;
+
+    public int damage;
+
+    List<ParticleCollisionEvent> colEvents = new List<ParticleCollisionEvent>();
+
+    
+    private void OnParticleCollision(GameObject other)
+    {
+        int events = enemyParticleAttack.GetCollisionEvents(other, colEvents);
+
+        Debug.Log("Hit");
+
+        other.gameObject.GetComponent<HealthBehaviour>().Hurt(damage);
+    }
+}
