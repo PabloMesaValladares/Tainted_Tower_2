801b99ad09e997aa0a4ca643f923d079df4a31ec
Crashso23 <104057891+TheRotzNPinkln@users.noreply.github.com>
2023-05-02 07:58:11 +0200

    noup

--- Assets/Scripts/Enemy/Kamikaze.cs
+++ Assets/Scripts/Enemy/Kamikaze.cs
@@ -48,10 +48,13 @@ public class Kamikaze : MonoBehaviour
 
             if (distanceBoom <= maxDistanceBoom)
             {
+                expire();
+                /*
                 boom.transform.position = gameObject.transform.position;
                 boom.SetActive(true);
                 cooldown = maxCooldown;
                 gameObject.SetActive(false);
+                */
             }
         }
     }
@@ -70,66 +73,15 @@ public class Kamikaze : MonoBehaviour
         if(objectiveConfirmed == false)
         {
             oldPlayerPosition = new Vector3(player.transform.position.x, gameObject.transform.position.y, player.transform.position.z);
-            //oldPlayerPosition = new Vector3(player.transform.position.x - gameObject.transform.position.x, player.transform.position.y - gameObject.transform.position.y, player.transform.position.z - gameObject.transform.position.z);
         }
     }
 
-
-
-    /*
-    [SerializeField]
-    private MovementBehavior _movement;
-
-    [SerializeField]
-    private Rigidbody _rigid;
-
-    [SerializeField]
-    private float vel, attackVel, distanceBoom;
-    [SerializeField]
-    private bool fighting;
-    [SerializeField]
-    private Vector3 oldPlayerPosition;
-    [SerializeField]
-    private GameObject _boomParticles;
-
-
-    [SerializeField]
-    private GameObject player, enemy;
-    // Start is called before the first frame update
-    void Awake()
+    public void expire()
     {
-        _rigid.GetComponent<Rigidbody>();
-        enemy = this.gameObject;
-        player = GameObject.FindGameObjectWithTag("Player");
+        boom.transform.position = gameObject.transform.position;
+        boom.SetActive(true);
+        cooldown = maxCooldown;
+        oldPlayerPosition = new Vector3(0, 0, 0);
+        gameObject.SetActive(false);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(fighting)
-        {
-            distanceBoom = Vector3.Distance(oldPlayerPosition, transform.position);
-
-            if(distanceBoom <= 2)
-            {
-                _boomParticles = PoolingManager.Instance.GetPooledObject("boomPar");
-                _boomParticles.transform.position = gameObject.transform.position;
-                _boomParticles.SetActive(true);
-                enemy.SetActive(false);
-            }
-
-            _rigid.AddForce(transform.forward * attackVel, ForceMode.Force);
-
-        }
-    }
-
-    public void AttackCheck()
-    {
-        transform.LookAt(player.transform.position);
-        _rigid.velocity = Vector3.zero;
-        oldPlayerPosition = new Vector3(player.transform.position.x , transform.position.y, player.transform.position.z);
-        fighting = true;
-    }
-
-    */
 }
