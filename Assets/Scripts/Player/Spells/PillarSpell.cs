e7430cc3af5b334819a4da8c2aa7fd07074684be
BabyScylla69 <alvarosilvalobos99@gmail.com>
2023-05-02 13:13:22 +0200

    2/05/2023

--- Assets/Scripts/Player/Spells/PillarSpell.cs
+++ Assets/Scripts/Player/Spells/PillarSpell.cs
@@ -4,25 +4,76 @@ using UnityEngine;
 
 public class PillarSpell : MonoBehaviour
 {
-    [SerializeField] float distance;
-
+    [SerializeField] float rayDistance, distance;
+    [SerializeField] bool previewB;
     public LayerMask markable;
     float markedLayer;
 
+    Transform raycastPoint;
+
+    MarkEnemy mark;
+
+    GameObject preview;
+
     void Start()
     {
+        mark = GetComponent<MarkEnemy>();
+
+        // preview = PoolingManager.Instance("PillarPrev");
+
         var rawValue = markable.value;
         var layerValue = Mathf.Log(rawValue, 2);
         markedLayer = layerValue;
+        previewB = false;
     }
 
     void Update()
     {
-        
+        if (!mark.marking)
+        {
+            
+        }
+        else
+        {
+            if (!previewB)
+                PreviewState(previewB);
+            else
+            {
+                RaycastInfo();
+                PreviewState(true);
+                preview.transform.position = raycastPoint.position;
+                previewB = true;
+            }
+        }
     }
 
-    public void RaycastDistance()
+    public void RaycastInfo()
     {
-        RaycastHit hit;
+        Transform camera = Camera.main.transform;
+
+        if (Physics.Raycast(camera.position, camera.forward, out RaycastHit hit, rayDistance, (int)markedLayer))
+        {
+            raycastPoint.position = hit.point;
+        }
+    }
+
+    void SummonPilar()
+    {
+        GameObject pillar = PoolingManager.Instance.GetPooledObject("Pillar");
+
+        if (!mark.marking)
+        {
+
+        }
+        else
+        {
+            pillar.transform.position = raycastPoint.position;
+        }
+
+    }
+
+    void PreviewState(bool b)
+    {
+        preview.SetActive(b);
     }
 }
