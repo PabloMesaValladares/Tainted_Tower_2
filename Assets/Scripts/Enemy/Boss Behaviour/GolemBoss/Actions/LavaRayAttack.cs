d2a23da34eb99c710cb081183b9a43656129ed3d
Mario Marcos Navarro <mmarnav28@gmail.com>
2023-05-04 12:55:07 +0200

    Golem_Boss v1.0

---
+++ Assets/Scripts/Enemy/Boss Behaviour/GolemBoss/Actions/LavaRayAttack.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FSM;
+
+[CreateAssetMenu(menuName = "FSM/Carnation/Action/LavaRayAttack")]
+
+
+public class LavaRayAttack : Action
+{
+    public float lavaOffset;
+    float count;
+
+    public override void Act(Controller controller)
+    {
+        
+            GameObject lavaRay = PoolingManager.Instance.GetPooledObject("LavaRay");
+            lavaRay.GetComponent<particleSpawn>().particlePlay();
+            lavaRay.SetActive(true);
+            
+        
+
+        
+            count += Time.deltaTime;
+        
+    }
+
+    public override void RestartVariables()
+    {
+        count = 0;
+    }
+
+}
