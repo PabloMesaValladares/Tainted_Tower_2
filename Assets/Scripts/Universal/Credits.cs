0025297542c98a3563f7fc2a597de479a829a241
BabyScylla69 <alvarosilvalobos99@gmail.com>
2023-05-12 11:47:33 +0200

    yes v2

---
+++ Assets/Scripts/Universal/Credits.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Credits : MonoBehaviour
+{
+    Animator anim;
+    [SerializeField] float scrollSpeed;
+
+    public SceneController scene;
+    [SerializeField] string scName, soName;
+
+    PlayerInput _config;
+
+    InputAction esc;
+    InputAction space;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        anim.speed = scrollSpeed;
+        _config = GetComponent<PlayerInput>();
+
+        esc = _config.actions["Pause"];
+        space = _config.actions["Jump"];
+    }
+
+    void Update()
+    {
+        if(esc.triggered || space.triggered)
+        {
+            scene.StartLevel(scName);
+            scene.SetSongName(soName);
+        }
+    }
+}
